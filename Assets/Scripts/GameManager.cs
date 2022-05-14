91eeb19629eca045c64f3f45be6131dd743e9604
d3val <devalt5008@gmail.com>
2022-05-14 18:53:43 -0500

    Scene selector UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Start()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public void LoadScene(int buildIndex)
+    {
+        if (buildIndex == SceneManager.GetActiveScene().buildIndex)
+            return;
+        SceneManager.LoadScene(buildIndex);
+    }
+}
---
+++ Assets/Scripts/GeneralUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralUI : MonoBehaviour
+{
+    public void ChangeScene(int sceneIndex)
+    {
+        GameManager.Instance.LoadScene(sceneIndex);
+    }
+}
